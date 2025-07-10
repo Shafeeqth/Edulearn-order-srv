1187a944c3776bd8a22be30d6295f7906b893699
shafeeque <shafeeqsha06@gmail.com>
2025-07-10 22:35:31 +0530

    feat: add redis  interface

---
+++ src/application/interfaces/redis_interface.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from typing import Optional, Any, AsyncContextManager
+
+from redis.asyncio import client
+
+class RedisInterface(ABC):
+    @property
+    @abstractmethod
+    def client(self) -> client.Redis:
+        pass
+    
+    @abstractmethod
+    async def get(self, key: str) -> Optional[bytes]:
+        pass
+
+    @abstractmethod
+    async def set(self, key: str, value: Any, expire: int | None) -> None:
+        pass
+
+    @abstractmethod
+    async def delete(self, key: str) -> None:
+        pass
+
+    @abstractmethod
+    def lock(self, key: str, timeout: int = 10) -> AsyncContextManager:
+        pass
+
+    @abstractmethod
+    async def close(self) -> None:
+        pass
+    
+    
+    
