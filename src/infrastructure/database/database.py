fcf14120f8d92ee284b9c005e0e922a44fd68e0d
shafeeque <shafeeqsha06@gmail.com>
2025-10-02 23:03:32 +0530

    feat(database): add database configurations and session setup

---
+++ src/infrastructure/database/database.py
@@ -0,0 +1,38 @@
+from contextlib import asynccontextmanager
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from src.infrastructure.config.settings import settings
+
+engine = create_async_engine(
+    settings.DATABASE_URL_ASYNC,
+    pool_size=settings.MAX_CONNECTIONS,
+    max_overflow=20,
+    pool_timeout=30,
+    echo=True,
+)
+
+
+
+AsyncSessionFactory = async_sessionmaker(
+    bind=engine,
+    autoflush=False,
+    expire_on_commit=False,
+    class_=AsyncSession,
+    autocommit=False,
+    future=True,
+    
+)
+Base = declarative_base()
+
+@asynccontextmanager
+async def get_db() :
+    async with AsyncSessionFactory() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception as e:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
